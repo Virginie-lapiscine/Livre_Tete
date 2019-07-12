ff1c0c63a53fb9ff04159b977e64b5077a230d26
Virginie <virginie.meymat@lapiscine.pro>
2019-07-12 10:45:50 +0200

    suppr use Request, classe non utilisée

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LivreController extends AbstractController
